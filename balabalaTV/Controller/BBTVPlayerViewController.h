49bb0b33721990da15877afef378859a0fab52e4
docee <docee@163.com>
2014-12-07 12:10:03 +0800

    初始化代码

---
+++ balabalaTV/Controller/BBTVPlayerViewController.h
@@ -0,0 +1,14 @@
+//
+//  BBTVPlayerViewController.h
+//  balabalaTV
+//
+//  Created by 黄盼青 on 14/12/6.
+//  Copyright (c) 2014年 docee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface BBTVPlayerViewController : MPMoviePlayerViewController
+
+@end
---
+++ balabalaTV/Controller/BBTVPlayerViewController.m
@@ -0,0 +1,13 @@
+//
+//  BBTVPlayerViewController.m
+//  balabalaTV
+//
+//  Created by 黄盼青 on 14/12/6.
+//  Copyright (c) 2014年 docee. All rights reserved.
+//
+
+#import "BBTVPlayerViewController.h"
+
+@implementation BBTVPlayerViewController
+
+@end
